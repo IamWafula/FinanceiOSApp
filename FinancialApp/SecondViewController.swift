7075b967cdc4614ed96dc99d97b86b369d437be6
Ian Wafula <kni8@mail.com>
2022-03-23 02:10:30 +0300

    Initial Commit

---
+++ FinancialApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FinancialApp
+//
+//  Created by System Administrator on 23/03/2022.
+//  Copyright © 2022 System Administrator. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
